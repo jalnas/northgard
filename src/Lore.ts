1aa845b28767dd940f5e85b37fc252923169246c
jalnas <patrik.jalnas@gmail.com>
2024-09-06 12:28:01 +0200

    Break out lore descriptions to separate file

--- src/Lore.ts
+++ src/Lore.ts
@@ -21,30 +21,3 @@ export type LoreType =
 	| "Carpentry Mastery"
 	| "Negotiation"
 	| "Trading Caravan"
-
-export const LoreDescriptions: LoreMap<string> = {
-	"Sharp Axes": "Your Woodcutters will produce 20% more Wood.",
-	"Colonization": "Reduces the amount of Food necessary to colonize one zone by 30%.",
-	"Eradication": "Food Silos gain a 10% Food production bonus. Also reduces Food Silo upgrade cost by 50%.",
-	"Recruitment":
-		"Permanently increases population growth speed by 25%. You gain an additional 5% during 4 months for each unit from your clan killed by an enemy clan or sacrificed (max +75%).",
-	"Medicine": "Increases your Healer's healing speed by 50%.",
-	"Hearthstone": "Reduces extra Firewood consumption during winter by 50% and reduce winter Food penalty by 20%.",
-	"Shiny Happy People": "-20% required Happiness based on your population.",
-	"Weaponsmith": "Increases all your military units' attack power by 20%.",
-	"Fur Coats": "Your units no longer have reduced power during winter.",
-	"Defensive Strategy": "Increases you civilians' attack and your Defense Towers' resistance by 30%.",
-	"Military Strategy":
-		"Military units gain +5% attack for each type of friendly military unit in the zone (max +25%).",
-	"Feeling Safe": "Gain +3 Happiness if you have a Warchief and +1 Happiness per upgraded military camp.",
-	"Legendary Heroes": "Improves your Warchief's attack and defense by 25%.",
-	"Monster Slayer":
-		"Improves your military units' attack by 10% against Draconic units and by 30% against other mystical creatures. Also increases  Military Experience gains by 50% for each mystical creature killed.",
-	"Mining Efficiency": "Miners extract  ore from deposits 30% faster.",
-	"Shipbuilding": "Unlocks the Lighthouse.Increases Sailors' resource production by 25%.",
-	"Coinage": "Your Trade Routes produce +100% Kröwns.Your Merchants will produce 20% more Kröwns.",
-	"Erudition": "Increases Loremaster production by 40%. Your next Carved Stone is free.",
-	"Carpentry Mastery": "Reduces your buildings' upgrade costs by 20%.Reduces upkeep from upgraded buildings by 20%.",
-	"Negotiation": "Reduces prices on the Marketplace by 50% and stock replenishment is 20% faster.",
-	"Trading Caravan": "Trade Routes earn 20% more Kröwns. Relations with neutral factions improve 20% faster.",
-}
---
+++ src/data/LoreDescriptions.ts
@@ -0,0 +1,50 @@
+import { LoreMap } from "~/Lore.js"
+
+export const LoreDescriptions: LoreMap<string> = {
+	"Sharp Axes": "Your Woodcutters will produce 20% more Wood.",
+	"Colonization": "Reduces the amount of Food necessary to colonize one zone by 30%.",
+	"Eradication": "Food Silos gain a 10% Food production bonus. Also reduces Food Silo upgrade cost by 50%.",
+	"Recruitment":
+		"Permanently increases population growth speed by 25%. You gain an additional 5% during 4 months for each unit from your clan killed by an enemy clan or sacrificed (max +75%).",
+	"Medicine": "Increases your Healer's healing speed by 50%.",
+	"Hearthstone": "Reduces extra Firewood consumption during winter by 50% and reduce winter Food penalty by 20%.",
+	"Shiny Happy People": "-20% required Happiness based on your population.",
+	"Weaponsmith": "Increases all your military units' attack power by 20%.",
+	"Fur Coats": "Your units no longer have reduced power during winter.",
+	"Defensive Strategy": "Increases you civilians' attack and your Defense Towers' resistance by 30%.",
+	"Military Strategy":
+		"Military units gain +5% attack for each type of friendly military unit in the zone (max +25%).",
+	"Feeling Safe": "Gain +3 Happiness if you have a Warchief and +1 Happiness per upgraded military camp.",
+	"Legendary Heroes": "Improves your Warchief's attack and defense by 25%.",
+	"Monster Slayer":
+		"Improves your military units' attack by 10% against Draconic units and by 30% against other mystical creatures. Also increases Military Experience gains by 50% for each mystical creature killed.",
+	"Mining Efficiency": "Miners extract ore from deposits 30% faster.",
+	"Shipbuilding": "Unlocks the Lighthouse. Increases Sailors' resource production by 25%.",
+	"Coinage": "Your Trade Routes produce +100% Kröwns. Your Merchants will produce 20% more Kröwns.",
+	"Erudition": "Increases Loremaster production by 40%. Your next Carved Stone is free.",
+	"Carpentry Mastery": "Reduces your buildings' upgrade costs by 20%. Reduces upkeep from upgraded buildings by 20%.",
+	"Negotiation": "Reduces prices on the Marketplace by 50% and stock replenishment is 20% faster.",
+	"Trading Caravan": "Trade Routes earn 20% more Kröwns. Relations with neutral factions improve 20% faster.",
+
+	"Hibernation": "Your non-sick clan members health regenerates during winter in allied territory.",
+	"Harpoons": "Fish production is increased by 30% and 1 more Fisherman can be assigned.",
+	"Protector of the Land": "Gain +1 Happiness per zone with at least one military camp.",
+	"Shield Mastery": "Shield Bearers' weapon forge is free. Increases the resistance of all military units by 5%.",
+
+	"Food Preservation": "For each Food Silo, gain +3 Food. +5 Food if upgraded.",
+	"Hero Emblem": "Stone cost to build an Altar of Kings is reduced to 5. It gives +3 Happiness and produces Fame.",
+	"Young and Proud": "Increases your military units' attack power by 2% for every zone you control.",
+	"The Value of Great Deeds":
+		"Skald will produce +1.5 Kröwns. Skalds' tool improvement is free and forging time is divided by 2.",
+	"Glory of the Clan":
+		"Increases Fame by 30%. Increases gains from Trade Routes and Great Trade Routes by 10% each 200 Fame.",
+
+	"Spare Tools":
+		"Tool improvement for Farmers, Hunters and Fishermen costs no Kröwns or Iron, and forging time for those tools is divided by 2.",
+	"Amenities":
+		"Costs -50% to develop a zone. Periodically, a sheepfold containing 2 Sheeps makes a new Sheep appear.",
+	"Industrious": "Upgraded buildings producing Food and Happiness can be assigned an additional villager.",
+	"Wool Trade": "Sheeps in Sheepfold earn you +0.5 Kröwns. Sheeps can be sacrificed for 40 Kröwns.",
+	"Food Trade":
+		"Trading food via Trade Routes' gives +50% Kröwns and +70% on tiles with a natural food source. Trade routes with Jötunns earn you +50% more relationship.",
+}
