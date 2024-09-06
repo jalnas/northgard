53a7d34367309506852b0cc3c48fb08dd33d4ff6
jalnas <patrik.jalnas@gmail.com>
2024-09-06 12:21:35 +0200

    Add clan and feast count as properties of the state

--- src/State.ts
+++ src/State.ts
@@ -1,11 +1,14 @@
-import { JobMap } from "./Jobs"
-import { ResourceMap } from "./Resources"
-import { BuildingMap } from "./Buildings"
-import { LoreMap } from "./Lore"
+import { JobMap } from "./Jobs.js"
+import { ResourceMap } from "./Resources.js"
+import { BuildingMap } from "./Buildings.js"
+import { LoreMap } from "./Lore.js"
+import { ClanType } from "./Clans.js"
 
 export type State = {
+	clan: ClanType
 	resources: ResourceMap<number>
 	workers: JobMap<number>
 	buildings: BuildingMap<number>
 	lore: LoreMap<boolean>
+	feasts: number
 }
